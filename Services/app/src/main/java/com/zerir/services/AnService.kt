f5a5c53de03e866eda8d7982bb3a4f61240f038b
OmarZerir <omarzerir@gmail.com>
2021-12-06 14:36:29 +0300

    update service to stop thread when service stopped

--- Services/app/src/main/java/com/zerir/services/AnService.kt
+++ Services/app/src/main/java/com/zerir/services/AnService.kt
@@ -6,13 +6,27 @@ import android.os.IBinder
 
 class AnService : Service() {
 
+    private var isRunning = false
+    private lateinit var thread: Thread
 
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Thread {
-            while (true) println("Running")
-        }.start()
+        isRunning = true
+
+        thread = Thread {
+            while (isRunning) {
+                Thread.sleep(1000)
+                println("Running")
+            }
+        }
+        thread.start()
+
         return START_STICKY
+    }
+
+    override fun onDestroy() {
+        isRunning = false
+        super.onDestroy()
     }
 }
